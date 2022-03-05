90de0ecae47eea3bd7f1ff0f7185f1ff47ac8f24
Daniel Stephen <daniel.vergil2711@gmail.com>
2022-03-04 19:41:37 -0800

    Created DB Schema and seeds

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // {TODO: Add your MySQL password}
+    password: '',
+    database: 'inventory_db'
+  },
+  console.log(`Connected to the inventory_db database.`)
+);
+
+// Query database
+
+
+// Query database
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
